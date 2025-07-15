1d71cf176d0c3a1aaacfdd3ed2fe308fb82cdf27
neha763 <nehaambekar1998@gmail.com>
2025-07-15 22:10:15 +0530

    Added final frontend code

---
+++ frontend/src/components/environment.ts
@@ -0,0 +1,14 @@
+// export const environment = {
+//   production: true,  // ✅ Since you're deploying this
+//   apiBaseUrl: 'http://18.170.25.223:8080/api',  // 🔁 Replace with your backend IP/domain
+//   appName: 'HRMS',
+//   enableDebug: false
+// };
+
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:8080/api',
+  appName: 'HRMS',
+  enableDebug: true
+};
+ 
