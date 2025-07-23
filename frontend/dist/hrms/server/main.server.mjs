6e4302cc969f26eadad95828dcc2f7e5ecd3efea
Ubuntu <ubuntu@ip-172-31-34-138.us-east-2.compute.internal>
2025-07-23 09:44:51 +0000

    Initial commit from EC2

---
+++ frontend/dist/hrms/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{e as a}from"./chunk-37ES37PW.mjs";import"./chunk-RJPWRLKX.mjs";import"./chunk-LRR3PCWL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
